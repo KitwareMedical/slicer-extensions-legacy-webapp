87bca41dae44488a6bcf44e48ca2961023b93b83
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-09-01 22:25:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# slicer-extensions-legacy-webapp
+
+Flask web application implementing legacy REST API for Slicer applications supporting only `Midas_v1`.
+
+## Current deployments
+
+_Coming soon_
+
+## History
+
+The original implementation was based on the `slicerpackages` and `slicerappstore` midas plugins
+associated with the [midasplatform](https://github.com/midasplatform).
+
+## License
+
+It is covered by the Apache License, Version 2.0:
+
+https://www.apache.org/licenses/LICENSE-2.0
+
---
+++ server/__init__.py
@@ -0,0 +1,138 @@
+import flask
+
+from os import environ
+
+from flask import json, Flask, request, Response, stream_with_context
+from requests import get
+
+
+IS_DEV = environ["FLASK_ENV"] == "development"
+WEBPACK_DEV_SERVER_HOST = "http://localhost:8080"
+
+SLICER_PACKAGE_SERVER_APP_ID = "5f4474d0e1d8c75dfc705482"
+SLICER_PACKAGE_SERVER_HOST = "https://slicer-packages.kitware.com"
+
+
+app = Flask(__name__, static_folder='assets')
+
+
+def proxy(host, path):
+    # Adapted from https://ajhyndman.medium.com/hot-reloading-with-react-and-flask-b5dae60d9898
+    response = get(f"{host}{path}")
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+    headers = {
+        name: value
+        for name, value in response.raw.headers.items()
+        if name.lower() not in excluded_headers
+    }
+    return (response.content, response.status_code, headers)
+
+
+@app.route("/api/json")
+def retrieve_extension_metadata():
+    request = flask.request
+
+    extension_id = request.args.get("extension_id", None)
+    app.logger.info("extension_id [%s]" % extension_id)
+
+    arch = request.args.get("arch", None)
+    app.logger.info("arch [%s]" % arch)
+
+    operating_system = request.args.get("os", None)
+    app.logger.info("operating_system [%s]" % operating_system)
+
+    productname = request.args.get("productname", None)
+    app.logger.info("productname [%s]" % productname)
+
+    slicer_revision = request.args.get("slicer_revision", None)
+    app.logger.info("slicer_revision [%s]" % slicer_revision)
+
+    if extension_id is None and not all([arch, operating_system, productname, slicer_revision]):
+        return flask.abort(404)
+
+    # Retrieve extension metadata
+    url = "{host}/api/v1/app/{app_id}/extension".format(
+        host=SLICER_PACKAGE_SERVER_HOST,
+        app_id=SLICER_PACKAGE_SERVER_APP_ID)
+    params = {
+        "extension_id": extension_id,
+        "app_revision": slicer_revision,
+        "os": operating_system,
+        "arch": arch,
+        "baseName": productname
+    }
+    app.logger.info("GET {url}?{params}".format(url=url, params=params))
+    result = get(url, params)
+    if result.status_code not in (200, 201):
+        return flask.abort(404)
+    extension = result.json()[0]
+
+    return json.dumps({
+        "stat": "ok",
+        "code": "0",
+        "message": "",
+        "data": [{
+            "extension_id": extension["_id"],
+            "item_id": extension["_id"],
+            "arch": extension["meta"]["arch"],
+            "enabled": "1",
+            "category": extension["meta"]["category"],
+            "contributors": extension["meta"].get("contributors", ""),
+            "date_creation": extension["created"],
+            "description": extension["meta"]["description"],
+            "icon_url": extension["meta"]["icon_url"],
+            "homepage": extension["meta"]["homepage"],
+            "name": "{0}.tar.gz".format(extension["name"]),
+            "os": extension["meta"]["os"],
+            "productname": extension["meta"]["baseName"],
+            "repository_type": extension["meta"]["repository_type"],
+            "repository_url": extension["meta"]["repository_url"],
+            "revision": extension["meta"]["revision"],
+            "screenshots": extension["meta"]["screenshots"],
+            "slicer_revision": extension["meta"]["app_revision"],
+            }]
+        })
+
+
+@app.route("/download")
+def download_extension():
+    request = flask.request
+    extension_id = request.args.get("items", None)
+    if extension_id is None:
+        return flask.abort(404)
+
+    # Retrieve extension files
+    url = "{host}/api/v1/item/{extension_id}/files".format(
+        host=SLICER_PACKAGE_SERVER_HOST,
+        extension_id=extension_id)
+    app.logger.info("GET %s" % url)
+    result = get(url)
+    if result.status_code not in (200, 201):
+        return flask.abort(404)
+    file_id = result.json()[0]['_id']
+
+    # Set download URL
+    download_url = "{host}/api/v1/file/{file_id}/download".format(
+        host=SLICER_PACKAGE_SERVER_HOST,
+        file_id=file_id)
+    app.logger.info("GET %s" % download_url)
+
+    # Since existing release do not set the QNetworkRequest::FollowRedirectsAttribute
+    # to true, we explicitly stream the download through this server.
+    req = get(download_url, stream=True)
+    return Response(
+        stream_with_context(req.iter_content(chunk_size=1024)),
+        content_type=req.headers['content-type'])
+
+
+@app.route("/", defaults={"path": "index.html"})
+@app.route("/<path:path>")
+def getApp(path):
+    if IS_DEV:
+        return proxy(WEBPACK_DEV_SERVER_HOST, request.path)
+    return app.send_static_file(path)
